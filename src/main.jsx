76afe505b2f9cf5e7bf366e40617f92994e4007f
mauro-ezequiel <129565868+mauro-ezequiel@users.noreply.github.com>
2023-11-22 14:53:32 -0300

    add menu with react router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import { Home } from "./components/home.jsx";
+
+const router = createBrowserRouter([
+  { path: "/", element: <App />, errorElement: <h1>error</h1> },
+  {
+    path: "/products",
+    element: (
+      <>
+        <Home />
+      </>
+    ),
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
