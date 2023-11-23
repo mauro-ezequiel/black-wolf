344575536806007ead32f19e1c6a27c6534afa82
mauro-ezequiel <129565868+mauro-ezequiel@users.noreply.github.com>
2023-11-23 14:57:35 -0300

    Convert the first page to be responsive and improve styling.

--- src/App.jsx
+++ src/App.jsx
@@ -10,14 +10,14 @@ function App() {
     <>
       <Bar />
       <section className="dark">
-        <h2 className="title">BLACK WOLF</h2>
+        <h2 className="title t">BLACK WOLF</h2>
         <Home />
         <div className="container-title">
           <h3 className="title">IMPRESION 3D & ACCESORIOS GYM</h3>
         </div>
         <div>
           <div className="material">
-            <h3 className="tit">materiales:</h3>
+            <h3 className="title">materiales:</h3>
             <Material />
           </div>
         </div>
